c0560f809e1f047d9af7b69d94ec23cc877dff61
vobidniak <vobidniak@homeaway.com>
2018-06-26 12:53:25 +0300

    find and add  i2c device

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,81 @@
+
 #include <Arduino.h>
 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
- 
-MDNSResponder mdns;
- 
-// впишите сюда данные, соответствующие вашей сети:
-const char* ssid = "ASUS";
-const char* password = "sorenstr";
- 
-ESP8266WebServer server(80);
+#include <Wire.h>
+#include "PCA9685.h"
 
-String webPage = "";
-String json = "";
- 
-int gpio0_pin = 0;
-int gpio2_pin = 2;
- 
-void setup(void){
-  webPage += "<h1>Hello World</h1>";
- 
- json += 
-      "{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}";
- 
-  delay(1000);
+
+PCA9685 pwmController;
+
+uint8_t portArray[] = {16, 5, 4, 0, 2, 14, 12, 13};
+String portMap[] = {"GPIO16", "GPIO5", "GPIO4", "GPIO0", "GPIO2", "GPIO14", "GPIO12", "GPIO13"};
+
+
+void check_if_exist_I2C() {
+  byte error, address;
+  int nDevices;
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0){
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknow error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  } //for loop
+  if (nDevices == 0)
+    Serial.println("No I2C devices found");
+  else
+    Serial.println("**********************************\n");
+  //delay(1000);           // wait 1 seconds for next scan, did not find it necessary
+}
+
+void scanPorts() { 
+  for (uint8_t i = 0; i < sizeof(portArray); i++) {
+    for (uint8_t j = 0; j < sizeof(portArray); j++) {
+      if (i != j){
+        Serial.print("Scanning (SDA : SCL) - " + portMap[i] + " : " + portMap[j] + " - ");
+        Wire.begin(portArray[i], portArray[j]);
+        check_if_exist_I2C();
+      }
+    }
+  }
+}
+
+
+void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
- 
-  // ждем соединения:
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");  //  "Подключились к "
-  Serial.println(ssid);
-  Serial.print("IP address: ");  //  "IP-адрес: "
-  Serial.println(WiFi.localIP());
- 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-               //  "Запущен MDNSresponder"
-  }
- 
-  server.on("/hello", [](){
-    server.send(200, "text/html", webPage);
-  });
-
-    server.on("/json", [](){
-    server.send(200, "application/json", json);
-  });
-
-
-  server.begin();
-  Serial.println("HTTP server started");
+  delay(5000);
+  Serial.println("\n\nI2C Scanner to scan for devices on each port pair D0 to D7");
+  delay(5000);
+  scanPorts();
+   delay(1000);
+   Wire.begin(2, 14);
+   Wire.setClock(400000);
+   delay(1000);
+    pwmController.resetDevices();      
+    pwmController.init(B000000);       
+   pwmController.printModuleInfo();
 }
- 
-void loop(void){
-  server.handleClient();
+
+void loop() {
+  delay(2000);
+   Serial.println("all ok");
 }
+
+
+
