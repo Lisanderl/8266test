1ff63b4adff9aa3a9501399f1f5e4c62261c2b43
vobidniak <vobidniak@homeaway.com>
2018-08-21 18:22:46 +0300

    add server config

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 
 #include <Arduino.h>
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
 #include <Wire.h>
 #include "PCA9685.h"
 #include "Pad.h"
 #include "MoveController.h"
+
+//WIFI CONFIG
+const char* ssid = "Lisanderl";
+const char* password = "12345";
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!!!!!!!!!!!");
+}
 
 
 PCA9685 pwmController;
@@ -13,12 +26,19 @@ PCA9685_ServoEvaluator pwmServo1(97, 302, 507);
 
 Pad simplePad1(pwmController, pwmServo1, 0, 1);
 Pad simplePad2(pwmController, pwmServo1, 2, 3);
-Pad simplePad3(pwmController, pwmServo1, 4, 5);
-Pad simplePad4(pwmController, pwmServo1, 6, 7);
+
+
 
 void setup() {
   Serial.begin(115200);
-   delay(1000);
+  delay(1000);
+  WiFi.mode(WIFI_AP);           //Only Access point
+  WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
+  IPAddress myIP = WiFi.softAPIP(); //Get IP address
+  Serial.print("HotSpt IP:");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
    Wire.begin(2, 14);
    Wire.setClock(400000);
    delay(1000);
@@ -28,6 +48,7 @@ void setup() {
 }
 
 void loop() {
+   server.handleClient();
   Serial.println("all ok");
 simplePad1.smartHorisontalMove(40);
  delay(2000);
