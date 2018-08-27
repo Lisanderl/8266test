8b8674f2845df178dcf7c2a9fb44b7fef1eac092
vobidniak <vobidniak@homeaway.com>
2018-08-27 18:59:50 +0300

    add web

---
+++ data/logic.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	
+	
+	//logic for buttons on UI
+$( "#straight" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+$( "#back" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+$( "#left" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+$( "#right" ).click(function() {
+  alert( "Handler for .click() called." );
+});
+
+});
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,20 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include "index.h"
 
 #include <Wire.h>
 #include "PCA9685.h"
 #include "Pad.h"
 #include "MoveController.h"
 
-//WIFI CONFIG
-const char* ssid = "Lisanderl";
-const char* password = "12345";
 ESP8266WebServer server(80);
+IPAddress myIP;
 
 void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!!!!!!!!!!!");
+  String s = MAIN_page;
+  server.send(302, "text/html", s);
 }
 
 
@@ -27,18 +26,19 @@ PCA9685_ServoEvaluator pwmServo1(97, 302, 507);
 Pad simplePad1(pwmController, pwmServo1, 0, 1);
 Pad simplePad2(pwmController, pwmServo1, 2, 3);
 
-
-
 void setup() {
   Serial.begin(115200);
+  delay(3000);
+ 
   delay(1000);
   WiFi.mode(WIFI_AP);           //Only Access point
-  WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
-  IPAddress myIP = WiFi.softAPIP(); //Get IP address
+  Serial.println(WiFi.softAP("Lisanderl", "1234567890") ? "Ready" : "Failed!");
+  myIP = WiFi.softAPIP(); //Get IP address
   Serial.print("HotSpt IP:");
   Serial.println(myIP);
   server.on("/", handleRoot);
   server.begin();
+
    Wire.begin(2, 14);
    Wire.setClock(400000);
    delay(1000);
@@ -48,16 +48,23 @@ void setup() {
 }
 
 void loop() {
+  delay(1000);
    server.handleClient();
-  Serial.println("all ok");
-simplePad1.smartHorisontalMove(40);
- delay(2000);
-simplePad1.horisontalMove(-60);
- delay(5000);
-simplePad2.smartHorisontalMove(35);
- delay(2000);
-simplePad2.horisontalMove(-55);
- delay(5000);
+   Serial.println("all ok");
+   Serial.print("My ip : ");
+   Serial.print(myIP);
+   Serial.println();
+   Serial.print("Connected : ");
+   Serial.print(WiFi.softAPgetStationNum());
+   Serial.println();
+// simplePad1.smartHorisontalMove(40);
+//  delay(2000);
+// simplePad1.horisontalMove(-60);
+//  delay(5000);
+// simplePad2.smartHorisontalMove(35);
+//  delay(2000);
+// simplePad2.horisontalMove(-55);
+//  delay(5000);
 }
 
 
