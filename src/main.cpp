b66c4216139e9406b95e9005d671ab9ae5a957c2
oshevchuk <oshevchuk@softserveinc.com>
2018-11-16 17:45:44 +0200

    bad test

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <ESP8266WebServer.h>
 #include <FS.h> 
 #include <ArduinoJson.h> 
-#include <MPU6050.h> 
+#include "MPU6050_6Axis_MotionApps20.h"
 
 #include <Wire.h>
 #include <PCA9685.h>
@@ -13,14 +13,67 @@
 #include "Pad.h"
 #include "MoveController.h"
 
-MPU6050 accelgyro;
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+MPU6050 mpu;
 
-    // int const  MAX_VAL = 507;
-    // int const  AVARAGE_VAL = 302;
-    // int const  LOW_VAL = 97;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+int const INTERRUPT_PIN = 7;
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+void accelConfig(){
+
+  Serial.println("Initializing I2C devices...");
+   pinMode(INTERRUPT_PIN, INPUT);
+  mpu.initialize();
+  delay(1000);
+  Serial.println("Testing device connections...");
+  Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+ devStatus = mpu.dmpInitialize();
+   // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MPU control/status vars
 String const name = "name";
 String const steps = "steps";
 String const actionId = "actionId";
@@ -137,16 +190,11 @@ void setup() {
   SPIFFS.begin();
   restConfig();
 
-   Wire.begin(2, 14);
-   Wire.setClock(400000);
-     delay(1000);
-    Serial.println("Initializing I2C devices...");
-    accelgyro.initialize();
-     delay(1000);
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-   delay(1000);
+  Wire.begin(2, 14);
+  Wire.setClock(400000);
+  delay(1000);
+  accelConfig();
+  delay(1000);
   //  pwmController.resetDevices();      
   //  pwmController.init(B000000);       
   //  pwmController.setPWMFrequency(50);
@@ -154,23 +202,63 @@ void setup() {
    moveController = new MoveController(pwmController, leftServo, rightServo, 2);
    delay(500);
    moveController->defaultPosition(true);
+   delay(3000);
+   Serial.println("Start working........");
+}
+
+
+void mpu_loop()
+{
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  if (!mpuInterrupt && fifoCount < packetSize) return;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+    // display quaternion values in easy matrix form: w x y z
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    Serial.print("quat\t");
+    Serial.print(q.w);
+    Serial.print("\t");
+    Serial.print(q.x);
+    Serial.print("\t");
+    Serial.print(q.y);
+    Serial.print("\t");
+    Serial.println(q.z);
+
+  }
 }
 
 void loop() {
  // server.handleClient();
 
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  
-   
-        Serial.print("a/g:\t");
-        Serial.print(ax); Serial.print("\t");
-        Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
-        Serial.println(gz);
- delay(3000);
+ mpu_loop();
 }
 
 
